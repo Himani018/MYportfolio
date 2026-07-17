81567061cacb207fbb81ec8a091541a9105f3cdd
Himani018 <himanibahuguna69@gmail.com>
2026-07-17 22:46:38 +0530

    edited navbar scroll behaviour and UI

--- vite-project/src/components/hero.jsx
+++ vite-project/src/components/hero.jsx
@@ -5,11 +5,12 @@ function Hero() {
             <h1 className="heroText">Hi, i'am <span className="name"> Himani Bahuguna.</span></h1>
             <h2 className="heroText">Code is my language. </h2>
             <h3 className="heroText">Creativity is my <span className="name">signature.</span></h3>
-            <p>BCA student ·<span id="Dynamic">Aspiring Software Developer .</span> 
-                 I turn curious ideas into fast, elegant
+            <p>BCA student ·<span id="Dynamic">Aspiring Software Developer .</span> </p>
+            <p>   I turn curious ideas into fast, elegant
                  web apps — Building modern web 
                  experiences with a focus on performance, 
                  scalability, and clean architecture.</p>
+            <button id="ProjectButton">View Project</button> <span> <button id="ResumeButton">Download Resume</button> </span>
         </>
     )
 }
