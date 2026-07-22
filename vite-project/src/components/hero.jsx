77422427e0342c79de6c784dba22fd457b06157c
Himani018 <himanibahuguna69@gmail.com>
2026-07-22 22:56:34 +0530

    hero section updated

--- vite-project/src/components/hero.jsx
+++ vite-project/src/components/hero.jsx
@@ -11,7 +11,10 @@ function Hero() {
                  experiences with a focus on performance, 
                  scalability, and clean architecture.</p>
             <button id="ProjectButton"> <FaFolderOpen /> View Project</button> <span> <button id="ResumeButton"> <FaDownload /> Download Resume</button> </span>
-            <button id="git"><FaGithub /> Github</button>
+            <button id="git" onClick={()=>window.open("https://github.com/Himani018")}><FaGithub /> Github</button>
+            
+            <p id="cgpa">9.04 <span>100+</span> </p>
+            <p id="Textundercgpa">Current CGPA <span>LeetCode Solved</span></p>            
         </>
     )
 }
